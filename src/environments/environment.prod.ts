430859142559dc2baa714fb97bf7e29369ae47ea
Julio <julioisaias@gmail.com>
2020-02-16 22:38:49 -0300

    master

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCuDhz5bjL5xth6eJUG3gQCLVmPMocYsRE',
+    authDomain: 'mixtosys.firebaseapp.com',
+    databaseURL: 'https://mixtosys.firebaseio.com',
+    projectId: 'mixtosys',
+    storageBucket: 'mixtosys.appspot.com',
+    messagingSenderId: '1032974444818'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: 'AIzaSyCuDhz5bjL5xth6eJUG3gQCLVmPMocYsRE',
     authDomain: 'mixtosys.firebaseapp.com',
