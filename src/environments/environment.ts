18983be9e3835301e9c9b7b7a765a252c026fdea
Julio Isaías <julioisaias@gmail.com>
2021-09-25 15:31:41 +0000

    Removed private Api Key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCuDhz5bjL5xth6eJUG3gQCLVmPMocYsRE',
-    authDomain: 'mixtosys.firebaseapp.com',
-    databaseURL: 'https://mixtosys.firebaseio.com',
-    projectId: 'mixtosys',
-    storageBucket: 'mixtosys.appspot.com',
-    messagingSenderId: '1032974444818'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
 
